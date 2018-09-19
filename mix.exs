9d7bec694e37c97a8b159b8e003dec2934bed339
Eric Watson <wasnotrice@gmail.com>
2018-09-18 22:55:45 -0500

    Add dialyzer and credo

--- mix.exs
+++ mix.exs
@@ -36,6 +36,8 @@ defmodule ScenicToggle.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 0.10", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.3", only: :dev, runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:scenic, @scenic_version}
 
